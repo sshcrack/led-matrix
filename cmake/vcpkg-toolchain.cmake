b5d3990536ce020fe9b0a0718b597f233edf3c43
sshcrack <34072808+sshcrack@users.noreply.github.com>
2025-03-12 11:43:01 +0100

    use env variables

--- cmake/vcpkg-toolchain.cmake
+++
@@ -1,6 +0,0 @@
-# Include the vcpkg toolchain file
-if(DEFINED ENV{VCPKG_ROOT})
-  include("$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
-else()
-  message(FATAL_ERROR "VCPKG_ROOT environment variable is not defined. Please set it to your vcpkg installation directory. For install instructions: https://github.com/Microsoft/vcpkg")
-endif()
