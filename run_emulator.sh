2de60330cfd8d73b3010cde2128321c7ba4ce4fc
sshcrack <34072808+sshcrack@users.noreply.github.com>
2025-03-12 15:47:02 +0100

    add .env file support for run_emulator.sh

--- run_emulator.sh
+++ run_emulator.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+if [  -f ".env" ]; then
+    echo "Environment file found at '.env'. Sourcing..."
+    set -a && source .env && set +a
+else
+    echo "Environment file '.env' not found. You can create one if you want"
+fi
+
+
 cmake --build build --target install
 ./emulator_build/install/main --led-chain 2 --led-parallel 2 --led-rows 64 --led-cols 64 --led-emulator --led-emulator-scale=4
