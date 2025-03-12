8639a1a73e0abd79bd5a39e8fbec48b997d17ee6
sshcrack <34072808+sshcrack@users.noreply.github.com>
2025-03-12 15:42:22 +0100

    change directory of run script

--- run_emulator.sh
+++ run_emulator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cmake --build build --target install
-./build/install/main --led-chain 2 --led-parallel 2 --led-rows 64 --led-cols 64 --led-emulator --led-emulator-scale=4
+./emulator_build/install/main --led-chain 2 --led-parallel 2 --led-rows 64 --led-cols 64 --led-emulator --led-emulator-scale=4
