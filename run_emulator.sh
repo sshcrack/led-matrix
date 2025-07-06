d3c7a841d13ec64bae4fc4e01f65feeb35ada2eb
Hendrik <34072808+sshcrack@users.noreply.github.com>
2025-07-07 01:28:47 +0200

    Exit emulator if build wasn't successful

--- run_emulator.sh
+++ run_emulator.sh
@@ -9,4 +9,10 @@ fi
 
 
 cmake --build emulator_build --target install
+if [ $? -ne 0 ]; then
+    echo "Build failed. Please check the output for errors."
+    exit 1
+fi
+
+echo "Build successful. Running the emulator..."
 ./emulator_build/install/main --led-chain 2 --led-parallel 2 --led-rows 64 --led-cols 64 --led-emulator --led-emulator-scale=4
