92f1147965f61e1945c74bca18aa76f8e6725f1b
Hendrik Lind <hendrik@sshcrack.me>
2026-06-18 19:07:15 +0200

    install deb file for build_upload.sh

--- build_upload.sh
+++ build_upload.sh
@@ -2,7 +2,10 @@
 set -e
 
 cmake --preset cross-compile
-cmake --build --preset cross-compile --target install
+cmake --build --preset cross-compile --target package
 
-rsync -avz --rsync-path="sudo rsync" build/install/ ledmat:/usr/
+deb=$(ls -t build/*.deb | head -1)
+echo "Installing $deb on ledmat..."
+rsync -avz "$deb" ledmat:/tmp/
+ssh ledmat sudo dpkg -i "/tmp/$(basename "$deb")"
 ssh ledmat sudo systemctl restart led-matrix.service
