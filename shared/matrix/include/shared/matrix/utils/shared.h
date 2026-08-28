07e3f22afc0a8d473f184dfa4522c8d1da6d2924
Hendrik Lind <hendrik@sshcrack.me>
2026-08-28 15:42:29 +0200

    fix(windows): export shared runtime globals

--- shared/matrix/include/shared/matrix/utils/shared.h
+++ shared/matrix/include/shared/matrix/utils/shared.h
@@ -3,7 +3,8 @@
 #include <atomic>
 #include "nlohmann/json.hpp"
 #include "shared/matrix/config/MainConfig.h"
+#include "shared/matrix/export.h"
 
-extern std::atomic<bool>  skip_image;
-extern std::atomic<bool>  exit_canvas_update;
-extern Config::MainConfig* config;
+extern SHARED_MATRIX_API std::atomic<bool> skip_image;
+extern SHARED_MATRIX_API std::atomic<bool> exit_canvas_update;
+extern SHARED_MATRIX_API Config::MainConfig* config;
